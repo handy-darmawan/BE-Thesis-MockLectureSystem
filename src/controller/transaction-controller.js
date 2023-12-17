acc4f6b62bf39e8d563be33cc0f4a8daf677949a
handyyy <dev.handy.darmawan@gmail.com>
2023-12-17 20:20:29 +0700

    pagination transaction done

--- src/controller/transaction-controller.js
+++ src/controller/transaction-controller.js
@@ -14,9 +14,15 @@ const getShifts = async (request, response, next) => {
 
 const getTransactions = async (request, response, next) => {
   try {
-    const transactions = await transactionService.getTransactions(request);
+    const results = await transactionService.getTransactions(request);
     response.status(200).json({
-      data: transactions,
+      data: results.listsOfTransactions.rows,
+      paging: {
+        page: results.page,
+        limit: results.limit,
+        total_page: results.totalPage,
+        total_item: results.totalRow,
+      }
     });
   } catch (error) {
     next(error);
